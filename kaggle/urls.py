2b26ffa5b12e3a05a0674de30667bd1e666a7867
pushkar <pushkarkolhe@gmail.com>
2014-02-27 12:04:23 -0800

    adds sort by

--- kaggle/urls.py
+++ kaggle/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from kaggle import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^leaderboard/(?P<order_by>\w*)$', views.leaderboard, name='leaderboard'),
     url(r'^cookie/$', views.cookie, name='cookie'),
     url(r'^form/$', views.form, name='form'),
     url(r'^submit/$', views.submit, name='submit'),
--- kaggle/views.py
+++ kaggle/views.py
@@ -4,23 +4,31 @@ from django.core.urlresolvers import reverse
 from kaggle.models import Student
 from django.template import RequestContext, loader
 
-def index(request):
-	leaderboard = Student.objects.order_by('score')
+def leaderboard(request, order_by):
+	display = "leaderboard"
+	if order_by == "name":
+		leaderboard = Student.objects.order_by('name')
+	elif order_by == "location":
+		leaderboard = Student.objects.order_by('location')
+	elif order_by == "last":
+		leaderboard = Student.objects.order_by('last_time')
+	else:
+		leaderboard = Student.objects.order_by('score')
 	output = ', '.join([p.name for p in leaderboard])
 	template = loader.get_template('kaggle/dashboard.html')
 	context = RequestContext(request, {
         'leaderboard': leaderboard,
+        'display': display
     })
 	return HttpResponse(template.render(context))
 
-def cookie(request):
-	return HttpResponse("Hellow")
-
 def form(request):
+	display = "form"
 	leaderboard = Student.objects.order_by('score')
-	template = loader.get_template('kaggle/form.html')
+	template = loader.get_template('kaggle/dashboard.html')
 	context = RequestContext(request, {
-        'leaderboard': leaderboard,
+		'leaderboard': leaderboard,
+        'display': display,
     })
 	return HttpResponse(template.render(context))
 
@@ -28,4 +36,7 @@ def submit(request):
 	s = get_object_or_404(Student, pk=request.POST['student_id'])
 	s.score = request.POST['student_score']
 	s.save()
-	return HttpResponseRedirect(reverse('kaggle:index'))
+	return HttpResponseRedirect(reverse('kaggle:leaderboard'))
+
+def cookie(request):
+	return HttpResponse("Hello, Cookie Page")
